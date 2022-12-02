f0e04691ce1e4dad2ec65fdd6572b533a48a22e2
yachu8174 <119028236+yachu8174@users.noreply.github.com>
2022-12-02 09:46:34 +0800

    up

--- Api/fff.py
+++ Api/fff.py
@@ -1,16 +1,13 @@
 from flask import Flask
 from flask import Flask, jsonify, render_template #模板套件,
 from flask import request #解析套件
-from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.config["JSON_AS_ASCII"] = False
-CORS(app, resources={r"./*":{"origins":["http://127.0.0.1:5500"]}})
 
-@app.route('/sss',methods=['GET'])
+@app.route('/',methods=['GET'])
 def home():
-    return render_template('../templates/main_page.html')
+    return render_template('/main_page.html')
 
 if __name__ == '__main__':
    app.run()
