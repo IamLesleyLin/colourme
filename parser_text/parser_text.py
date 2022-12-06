204d1e6ca8f85427f338f8aa71888defc9091755
IamLesleyLin <yahsuanlin66@gmail.com>
2022-12-06 09:42:10 +0800

    add list
    add list

--- parser_text/parser_text.py
+++ parser_text/parser_text.py
@@ -5,6 +5,15 @@ import time
 import pandas as pd
 import numpy as np
 
+df_50=pd.read_csv('df_50.csv')
+df_50
+
+select_list=[]
+for i in range(len(df_50)):
+    df_50.iat[i,0]
+    select_list.append(df_50.iat[i,0])
+select_list
+
 html=[]
 addidasUrl = 'https://www.adidas.com.tw/product/'
 for i in select_list:
