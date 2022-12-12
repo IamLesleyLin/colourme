21faf30595ce20b3683db0f36d7a31c913990dac
sn987532159 <sn987532159@gmail.com>
2022-12-13 01:25:52 +0800

    update api and websites

--- Api/API.py
+++ Api/API.py
@@ -1,12 +1,8 @@
 import base64
-
-from flask import Flask
 from flask import Flask, jsonify, render_template #模板套件,
 from flask import request #解析套件
 from flask_cors import CORS
 import pymysql
-import os
-import pathlib
 import cv2
 import numpy as np
 import re
